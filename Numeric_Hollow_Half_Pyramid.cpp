327600d99b6a61dfa28d12ecdace4c73d56af924
mandeep-chhabra <mandeepsinghchhabra.edu@gmail.com>
2024-02-01 07:27:06 +0530

    Initial Commit

---
+++ Numeric_Hollow_Half_Pyramid.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Numeric Hollow Half Pyramid
+//      1
+//      1 2
+//      1   3
+//      1     4
+//      1 2 3 4 5
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // Outer Loop - Row Observation
+    for (int row = 0; row < n; row++)
+    {
+        // Inner Loop - Column Observation
+        for (int col = 0; col < row + 1; col++)
+        {
+            if (row == 0 | row == n - 1)
+            {
+                cout << col + 1 << " ";
+            }
+            else
+            {
+                if (col == 0 | col == row)
+                {
+                    cout << col + 1 << " ";
+                }
+                else
+                {
+                    cout << "  ";
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
